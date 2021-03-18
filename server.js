ab15119fcefafa48e176826dbf59e62d184e3e5a
Will <willstone@gmail.com>
2021-03-18 03:11:18 +0000

    require and use body-parser, add new-user post route function

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser');
 require('dotenv').config()
 
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/exercise/new-user', function(req, res) {
+  console.log(req.body.url);
+  let host = req.body.url;
 
+});
 
 
 
